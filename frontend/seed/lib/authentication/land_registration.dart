89f699f0283e6d6e15f850a3ca65465bcc0f188e
silaschege <silaskimani4080@gmail.com>
2022-06-06 11:57:11 +0300

    django and flutter setup

---
+++ frontend/seed/lib/authentication/land_registration.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LandRegistration extends StatelessWidget {
+  const LandRegistration({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ frontend/seed/lib/authentication/password_managment.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PasswordManagment extends StatelessWidget {
+  const PasswordManagment({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
