8c058fa5327ae5c1804fe62e905445b3f0e3e239
silaschege <silaskimani4080@gmail.com>
2022-06-09 07:38:51 +0300

    login not yet done

--- frontend/seed/lib/authentication/login.dart
+++ frontend/seed/lib/authentication/login.dart
@@ -5,6 +5,79 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(color: Color.fromRGBO(83, 166, 98, 1)),
+        child: Column(
+          children: <Widget>[
+            Image.asset(
+              'images/logo.png',
+              height: 200,
+              width: 200,
+            ),
+            Expanded(
+                child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      topRight: Radius.circular(60))),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 60,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color.fromARGB(252, 213, 111, 1),
+                                blurRadius: 20,
+                                offset: Offset(0, 10))
+                          ]),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.grey))),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  hintText: "Email/Mobile Number",
+                                  hintStyle: TextStyle(
+                                      color: Color.fromARGB(252, 213, 111, 1)),
+                                  border: InputBorder.none),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.grey))),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  hintText: "Password",
+                                  hintStyle: TextStyle(
+                                      color: Color.fromARGB(252, 213, 111, 1)),
+                                  border: InputBorder.none),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ))
+          ],
+        ),
+      ),
+    );
   }
 }
